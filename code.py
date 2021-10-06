b9bcfa5a2d9e740abac26ee003b7f40e2b1b87f1
Igor-Zhelezniak-1 <90275833+Igor-Zhelezniak-1@users.noreply.github.com>
2021-10-06 13:12:14 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by: Igor
+# Created on: Oct 2021
+# This is code
+
+import ugame
+import stage
+
+
+def game_scene():
+    # This function is the main game game_scene
+    
+    print("\n\n\n")     # 3 blank lines
+    print("Hello, World!")
+    
+    # repeat forever, game loop
+    while True:
+        pass
+
+if __name__ == "__main__":
+    game_scene()
