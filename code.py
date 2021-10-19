a21c4c3ccc17d15950a9d371a77b04791ef53db6
Igor-Zhelezniak-1 <90275833+Igor-Zhelezniak-1@users.noreply.github.com>
2021-10-19 12:24:31 -0400

    Update code.py

--- code.py
+++ code.py
@@ -11,12 +11,17 @@ import stage
 def game_scene():
     # This function is the main game game_scene
     
-    print("\n\n\n")     # 3 blank lines
-    print("Hello, World!")
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+    background = stage.Grid(image_bank_background, 10, 8)
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [ship] + [background]
+    game.render_block()
     
     # repeat forever, game loop
     while True:
-        pass
+        game.render_sprites([ship])
+        game.tick()
 
 if __name__ == "__main__":
     game_scene()
