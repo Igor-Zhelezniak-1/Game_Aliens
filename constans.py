005decb3c49d5a2b83e5f18ea80d6f8581a598c4
Igor-Zhelezniak-1 <90275833+Igor-Zhelezniak-1@users.noreply.github.com>
2021-10-19 13:49:06 -0400

    Create constans.py

---
+++ constans.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Igor
+# Created on: Oct 2021
+# This is constans for game
+
+# PyBage screen size is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOUVMENT_SPEED = 1
