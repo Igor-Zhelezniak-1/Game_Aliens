dfff7b5f04622d0c6d6aa3f7b0bdc89583bcd1ac
Igor-Zhelezniak-1 <90275833+Igor-Zhelezniak-1@users.noreply.github.com>
2021-10-19 13:10:01 -0400

    Update code.py

--- code.py
+++ code.py
@@ -20,6 +20,25 @@ def game_scene():
     
     # repeat forever, game loop
     while True:
+        keys = ugame.buttons.get_pressed()
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
+        
+        
         game.render_sprites([ship])
         game.tick()
 
