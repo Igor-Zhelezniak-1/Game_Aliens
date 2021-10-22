e2738bd906f503c104de23b4d99f398da6b29726
Igor-Zhelezniak-1 <90275833+Igor-Zhelezniak-1@users.noreply.github.com>
2021-10-22 14:29:02 -0400

    Update constans.py

--- constans.py
+++ constans.py
@@ -13,3 +13,11 @@ SPRITE_SIZE = 16
 TOTAL_NUMBER_OF_ALIENS = 5
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
+
+# Using for button state
+button_state = {
+  "button_up": "up",
+  "button_just_pressed": "just pressed",
+  "button_still_pressed": "still pressed",
+  "button_released": "released"
+}
